706a2dc0049e41627546236c53e187edd843dcb9
Rituparna W <rituparnaw16@gmail.com>
2024-07-21 02:57:44 +0900

    epic(wish): setup bubbletea app over ssh

--- main.go
+++ main.go
@@ -1,12 +1,77 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+	"github.com/muesli/termenv"
+)
+
+const (
+	host = "0.0.0.0"
+	port = "42069"
 )
 
 func main() {
-	p := tea.NewProgram(NewModel(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		panic(err)
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			myCustomBubbleteaMiddleware(),
+			logging.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Error("Could not start server", "error", err)
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+func myCustomBubbleteaMiddleware() wish.Middleware {
+	newProg := func(m tea.Model, opts ...tea.ProgramOption) *tea.Program {
+		p := tea.NewProgram(m, opts...)
+		return p
+	}
+	teaHandler := func(s ssh.Session) *tea.Program {
+		_, _, active := s.Pty()
+		if !active {
+			wish.Fatalln(s, "no active terminal, skipping")
+			return nil
+		}
+
+		// renderer := bubbletea.MakeRenderer(s)
+
+		m := NewModel()
+		return newProg(m, append(bubbletea.MakeOptions(s), tea.WithAltScreen())...)
+	}
+	return bubbletea.MiddlewareWithProgramHandler(teaHandler, termenv.ANSI256)
 }
