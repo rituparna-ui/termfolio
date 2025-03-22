512f1d89c26b8f92b93907a063185d32d8ab7f9e
Rituparna Warwatkar <rituu@amazon.de>
2025-03-22 09:33:59 +0000

    feat(quit): add ctrl+c hotkey to quit

--- update.go
+++ update.go
@@ -3,5 +3,11 @@ package main
 import tea "github.com/charmbracelet/bubbletea"
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+		}
+	}
 	return m, nil
 }
