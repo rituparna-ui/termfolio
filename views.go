5f47bf83f719b227bc2823be3a68f69567a9f481
Rituparna Warwatkar <rituu@amazon.de>
2025-03-22 11:12:07 +0000

    chore(bootview): style boot view. todo- add loading bar

--- views.go
+++ views.go
@@ -5,14 +5,46 @@ import "github.com/charmbracelet/lipgloss"
 func (m Model) View() string {
 	switch m.Screen {
 	case BootView:
-		return GenerateBootView()
+		return GenerateBootView(&m)
 	default:
 		return GenerateNotFoundView(&m)
 	}
 }
 
-func GenerateBootView() string {
-	return "Boot View"
+func bootViewCenterTextBox() string {
+	line1 := "Hey There!"
+	line2 := "This is @rituu's Termfolio"
+	line3 := "A Terminal based Portfolio over SSH !!!"
+
+	return lipgloss.NewStyle().
+		Align(lipgloss.Center).
+		Padding(1, 2).
+		Foreground(lipgloss.Color("#ea76cb")).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#8839ef")).
+		Render(
+			lipgloss.JoinVertical(lipgloss.Center,
+				line1,
+				line2,
+				line3,
+			),
+		)
+}
+
+func GenerateBootView(m *Model) string {
+	centerTextBox := bootViewCenterTextBox()
+	booting := lipgloss.
+		NewStyle().
+		Blink(true).
+		Foreground(lipgloss.Color("#df8e1d")).
+		Render("Press Enter to continue ...")
+
+	return CenterTextStyle(m).Render(
+		lipgloss.JoinVertical(lipgloss.Center,
+			centerTextBox,
+			booting,
+		),
+	)
 }
 
 func GenerateNotFoundView(m *Model) string {
